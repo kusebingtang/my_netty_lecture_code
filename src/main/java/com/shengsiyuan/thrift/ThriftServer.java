0895dc1ddca25c6ae89f032d404b6612add21198
jbw <jb@98game.cn>
2019-11-13 17:12:10 +0800

    Apache Thrift应用详解与实例剖析

---
+++ src/main/java/com/shengsiyuan/thrift/ThriftServer.java
@@ -0,0 +1,32 @@
+package com.shengsiyuan.thrift;
+
+import org.apache.thrift.TProcessorFactory;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import thrift.generated.PersonService;
+
+public class ThriftServer {
+
+    public static void main(String[] args) throws Exception {
+
+        TNonblockingServerSocket serverSocket = new TNonblockingServerSocket(8899);
+
+        THsHaServer.Args thServerArgs = new THsHaServer.Args(serverSocket).minWorkerThreads(2).maxWorkerThreads(4);
+
+        PersonService.Processor<PersonServiceImpl> processor = new PersonService.Processor<>(new PersonServiceImpl());
+
+        thServerArgs.protocolFactory(new TCompactProtocol.Factory());
+        thServerArgs.transportFactory(new TFramedTransport.Factory());
+        thServerArgs.processorFactory(new TProcessorFactory(processor));
+
+        TServer server = new THsHaServer(thServerArgs);
+
+        System.out.println("Thrift Server Started!");
+
+        server.serve();
+
+    }
+}
