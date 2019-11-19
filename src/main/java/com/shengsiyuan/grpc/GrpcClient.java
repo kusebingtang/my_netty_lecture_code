f16d034c27c216e5d8f154a29dd9c139dd3a53d1
jbw <jb@98game.cn>
2019-11-19 11:10:27 +0800

    gRPC服务器流式调用实现

--- src/main/java/com/shengsiyuan/grpc/GrpcClient.java
+++ src/main/java/com/shengsiyuan/grpc/GrpcClient.java
@@ -4,6 +4,7 @@ package com.shengsiyuan.grpc;
 import com.shengsiyuan.proto.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 import java.util.Iterator;
 
@@ -29,6 +30,47 @@ public class GrpcClient {
             StudentResponse studentResponse = iterator.next();
             System.out.println(studentResponse.getName() + ", " + studentResponse.getAge() + ", " + studentResponse.getCity());
         }
+
+
+        System.out.println("--------------------");
+        StreamObserver<StudentResponseList> studentResponseListStreamObserver = new StreamObserver<StudentResponseList>() {
+            @Override
+            public void onNext(StudentResponseList value) {
+                value.getStudentResponseList().forEach(studentResponse -> {
+                    System.out.println(studentResponse.getName());
+                    System.out.println(studentResponse.getAge());
+                    System.out.println(studentResponse.getCity());
+                    System.out.println("******");
+                });
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("completed!");
+            }
+        };
+
+        StreamObserver<StudentRequest> studentRequestStreamObserver = stub.getStudentsWrapperByAges(studentResponseListStreamObserver);
+
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(20).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(30).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(40).build());
+        studentRequestStreamObserver.onNext(StudentRequest.newBuilder().setAge(50).build());
+
+        studentRequestStreamObserver.onCompleted();
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- src/main/java/com/shengsiyuan/grpc/StudentServiceImpl.java
+++ src/main/java/com/shengsiyuan/grpc/StudentServiceImpl.java
@@ -27,4 +27,32 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<StudentRequest> getStudentsWrapperByAges(StreamObserver<StudentResponseList> responseObserver) {
+         return new StreamObserver<StudentRequest>(){
+
+            @Override
+            public void onNext(StudentRequest value) {
+                System.out.println("onNext: " + value.getAge());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                StudentResponse studentResponse = StudentResponse.newBuilder().setName("张三").setAge(20).setCity("西安").build();
+                StudentResponse studentResponse2 = StudentResponse.newBuilder().setName("李四").setAge(30).setCity("广州").build();
+
+                StudentResponseList studentResponseList = StudentResponseList.newBuilder().
+                        addStudentResponse(studentResponse).addStudentResponse(studentResponse2).build();
+
+                responseObserver.onNext(studentResponseList);
+                responseObserver.onCompleted();
+            }
+        };
+    }
 }
