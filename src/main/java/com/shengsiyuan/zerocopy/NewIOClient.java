5730cd9bb1b5325a985073e1c54fb7c6a4617b7f
jbw <jb@98game.cn>
2020-03-03 15:54:17 +0800

    NIO zero copy

---
+++ src/main/java/com/shengsiyuan/zerocopy/NewIOClient.java
@@ -0,0 +1,27 @@
+package com.shengsiyuan.zerocopy;
+
+import java.io.FileInputStream;
+import java.net.InetSocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+public class NewIOClient {
+
+    public static void main(String[] args) throws Exception {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost", 8899));
+        socketChannel.configureBlocking(true);
+
+        String fileName = "/Users/zyh/Desktop/JVM/11_类加载器双亲委托机制详解.mp4";
+
+        FileChannel fileChannel = new FileInputStream(fileName).getChannel();
+
+        long startTime = System.currentTimeMillis();
+
+        long transferCount = fileChannel.transferTo(0, fileChannel.size(), socketChannel);
+
+        System.out.println("发送总字节数：" + transferCount + "，耗时： " + (System.currentTimeMillis() - startTime));
+
+        fileChannel.close();
+    }
+}
---
+++ src/main/java/com/shengsiyuan/zerocopy/OldIOClient.java
@@ -0,0 +1,35 @@
+package com.shengsiyuan.zerocopy;
+
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class OldIOClient {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 8899);
+
+        String fileName = "/Users/zyh/Desktop/JVM/11_类加载器双亲委托机制详解.mp4";
+        InputStream inputStream = new FileInputStream(fileName);
+
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        byte[] buffer = new byte[4096];
+        long readCount;
+        long total = 0;
+
+        long startTime = System.currentTimeMillis();
+
+        while ((readCount = inputStream.read(buffer)) >= 0) {
+            total += readCount;
+            dataOutputStream.write(buffer);
+        }
+
+        System.out.println("发送总字节数： " + total + ", 耗时： " + (System.currentTimeMillis() - startTime));
+
+        dataOutputStream.close();
+        socket.close();
+        inputStream.close();
+    }
+}
